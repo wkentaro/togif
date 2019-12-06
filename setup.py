68572c7ba5b190ffe9041d37cb5dfcb52449a7c0
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-06 20:26:53 +0000

    1.0.0

--- setup.py
+++ setup.py
@@ -3,28 +3,47 @@ from __future__ import print_function
 from setuptools import setup
 import shlex
 import subprocess
+import sys
 
 
-def git_hash():
-    cmd = 'git log -1 --format="%h"'
+version = '1.0.0'
+
+
+# release helper
+if sys.argv[-1] == 'release':
+    commands = [
+        'git pull origin master',
+        'git tag v{0}'.format(version),
+        'git push origin master --tags',
+        'python setup.py sdist',
+        'twine upload dist/togif-{0}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        print('+ {0}'.format(cmd))
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+def get_long_description():
+    with open('README.md') as f:
+        long_description = f.read()
+
     try:
-        hash_ = subprocess.check_output(shlex.split(cmd)).decode().strip()
-    except subprocess.CalledProcessError:
-        hash_ = None
-    return hash_
+        import github2pypi
 
-
-version = '0.1.0.post0'
-
-hash_ = git_hash()
-if hash_ is not None:
-    version = '%s.%s' % (version, hash_)
+        return github2pypi.replace_url(
+            slug='wkentaro/togif', content=long_description
+        )
+    except Exception:
+        return long_description
 
 
 setup(
     name='togif',
     version=version,
     py_modules=['togif'],
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     install_requires=['imageio<2.5', 'imageio-ffmpeg', 'imgviz', 'tqdm'],
     author='Kentaro Wada',
     author_email='www.kentaro.wada@gmail.com',
